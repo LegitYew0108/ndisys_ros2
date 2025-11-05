86a0e6bffa1c9cca1beb1c658509a6cf5c59878b
LegitYew0108 <legitlegit0108@gmail.com>
2025-11-05 18:14:11 +0900

    fix: add clean() to uncleaned frames vec

--- ndi_controllers/rigid_pose_broadcaster/src/rigid_pose_broadcaster.cpp
+++ ndi_controllers/rigid_pose_broadcaster/src/rigid_pose_broadcaster.cpp
@@ -133,6 +133,7 @@ RigidPoseBroadcaster::update(const rclcpp::Time & time, const rclcpp::Duration &
   {
     realtime_rigid_pose_publisher_->msg_.poses.clear();
     realtime_rigid_pose_publisher_->msg_.ids.clear();
+		realtime_rigid_pose_publisher_->msg_.frames.clear();
     realtime_rigid_pose_publisher_->msg_.inbound.clear();
 
     auto & rigidPoseMsg = realtime_rigid_pose_publisher_->msg_;
